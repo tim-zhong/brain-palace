3cf1c28b5d063facd1fe987f2eff9b9c0f1bb616
Tim Zhong <tim.zhong@ibm.com>
2017-07-25 15:00:01 -0400

    Allow client side access to static resources

--- bp-ui/index.js
+++ bp-ui/index.js
@@ -12,6 +12,9 @@ var welcome = require('./controllers/welcome');
 
 var app = express();
 app.engine('.dust', consolidate.dust);
+app.use('/', express.static(__dirname + '/public', {
+    redirect: false
+}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'dust');
 
